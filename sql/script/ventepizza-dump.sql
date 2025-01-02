61e66c0a20d845ca9ba39357e8a390f497708967
Kylian Dupouy Peron <kdperon@yahoo.com>
2025-01-02 14:31:03 +0100

    update

--- sql/script/ventepizza-dump.sql
+++ sql/script/ventepizza-dump.sql
@@ -93,7 +93,7 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-02 14:28:39
+-- Dump completed on 2025-01-02 14:30:59
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ventepizza
@@ -231,7 +231,7 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `getSaleLineById` */;
+/*!50003 DROP PROCEDURE IF EXISTS `getSalesLineById` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -241,7 +241,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getSaleLineById`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getSalesLineById`(
     IN _id INT
 )
 BEGIN
@@ -262,4 +262,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-02 14:28:39
+-- Dump completed on 2025-01-02 14:30:59
